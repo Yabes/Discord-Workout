fd8bccb902b036e4dd8572a12b7dc47e5d3cbb76
Matthis Lemaitre-Cosquer <yabesmirax@gmail.com>
2016-02-05 15:57:33 +0100

    Remove score saving on debug

--- src/score.js
+++ src/score.js
@@ -16,6 +16,8 @@ export default class Score {
    * @param {Exercice} exercice - exercice to save
    */
   static addScore (exercice) {
+    if (process.env.NODE_ENV === 'debug') return;
+
     if (!scores.players) scores.players = {};
     if (!scores.players[exercice.username]) scores.players[exercice.username] = { total: 0, detail: {PUSHUPS: 0, PLANK: 0, SITUPS: 0, WALLSIT: 0 }};
 
